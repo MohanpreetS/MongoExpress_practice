ec1fff454e9c39fe75dd68fd8ec2c4e537de1de5
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-06-01 13:09:04 -0400

    basic get requests

--- expressIntro/index.js
+++ expressIntro/index.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    console.log("WE GOT A NEW REQUEST!!")
-    res.send('<h1>This is my webpage!</h1>')
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST!!")
+//     res.send('<h1>This is my webpage!</h1>')
+// })
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the home page!')
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!')
 })
 
 app.listen(8080, () => {
