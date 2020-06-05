47642ed30d399778c322467e2e0acb8af6c5ca1a
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-06-05 13:09:04 -0400

    partials basics, and templating

--- templating/index.js
+++ templating/index.js
@@ -7,3 +7,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
