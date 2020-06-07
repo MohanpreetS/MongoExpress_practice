52ada3abdf1a9c01ec0d07dba914958f1f7351cc
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-06-07 13:09:04 -0400

    ejs syntax basics

---
+++ templating/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
+    ]
+    res.render('cats', { cats })
+})
+
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
