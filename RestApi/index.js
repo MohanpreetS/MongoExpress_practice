0d099ea5f619ee0bfaeb5be895222c038822159a
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-06-21 13:09:04 -0400

    get and post requests for tacors

--- RestApi/index.js
+++ RestApi/index.js
@@ -1,9 +1,21 @@
 const path = require('path');
-
+const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid'); //For generating ID's
 const express = require('express');
 const app = express();
 
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response")
+})
 
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat} tacos`)
+})
+
+app.listen(3000, () => {
+    console.log("ON PORT 3000!")
+})
 
 
 
