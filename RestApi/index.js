686636d4d4ff6cc35c451c206b4678fc91ebb9c9
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-06-29 13:09:04 -0400

    planning routes and making pseudo db

--- RestApi/index.js
+++ RestApi/index.js
@@ -4,6 +4,44 @@ const { v4: uuid } = require('uuid'); //For generating ID's
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+// GET /comments - list all comments
+// POST /comments - Create a new comment 
+// GET /comments/:id - Get one comment (using ID)
+// PATCH /comments/:id - Update one comment
+// DELETE /comments/:id - Destroy one comment
+
+
+// Our fake database:
+let comments = [
+    {
+        id: uuid(),
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        id: uuid(),
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        id: uuid(),
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        id: uuid(),
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
 })
@@ -16,6 +54,8 @@ app.post('/tacos', (req, res) => {
 app.listen(3000, () => {
     console.log("ON PORT 3000!")
 })
+
+
 
 
 
