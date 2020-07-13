8e041d14769b1668fc1beba63e2b26471184f7cd
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-07-13 13:09:04 -0400

    mongoose model/schema

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -8,6 +8,14 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
         console.log(err)
     })
 
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
 
 
 
