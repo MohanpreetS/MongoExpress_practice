ea93181636ff168f98bcaac7cd1f11249c88c9ac
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-07-15 13:09:04 -0400

    mongoose schema validation basics

---
+++ MongooseBasics/product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!")
+        console.log(err)
+    })
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, 'Price must be positive ya dodo!']
+    },
+});
+
