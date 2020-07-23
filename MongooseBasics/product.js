09cbb89bbad8c9e699afd5f4577e58f8a13192bb
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-07-23 13:09:04 -0400

    mongoose instance method basics

--- MongooseBasics/product.js
+++ MongooseBasics/product.js
@@ -41,25 +41,31 @@ const productSchema = new mongoose.Schema({
 
 });
 
-const bike = new Product({ name: 'Cycling Jersey', price: 28.50, categories: ['Cycling'], size: 'XS' })
-bike.save()
-    .then(data => {
-        console.log("IT WORKED!")
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!")
-        console.log(err)
-    })
+productSchema.methods.greet = function () {
+    console.log("HELLLO!!! HI!! HOWDY!!! ")
+    console.log(`- from ${this.name}`)
+}
 
-Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: 9 }, { new: true, runValidators: true })
-    .then(data => {
-        console.log("IT WORKED!")
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("OH NO ERROR!")
-        console.log(err)
-    })
+
+// const bike = new Product({ name: 'Cycling Jersey', price: 28.50, categories: ['Cycling'], size: 'XS' })
+// bike.save()
+//     .then(data => {
+//         console.log("IT WORKED!")
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("OH NO ERROR!")
+//         console.log(err)
+//     })
+
+// Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: 9 }, { new: true, runValidators: true })
+//     .then(data => {
+//         console.log("IT WORKED!")
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("OH NO ERROR!")
+//         console.log(err)
+//     })
 
 
