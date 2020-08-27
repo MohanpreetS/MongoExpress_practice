2a92fb7b1ca551214d9aea704b0c051d67635001
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-08-27 13:09:04 -0400

    edit product api

--- mongooseExpress/index.js
+++ mongooseExpress/index.js
@@ -58,6 +58,12 @@ app.get('/products/:id/edit', async (req, res) => {
     res.render('products/edit', { product, categories })
 })
 
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+})
+
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
