f924fb5c9134de09f76a9b453584cb6c0eb6e076
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-10-01 13:09:04 -0400

    simple custom middleware

--- middlewareIntro/index.js
+++ middlewareIntro/index.js
@@ -5,6 +5,11 @@ const morgan = require('morgan');
 
 app.use(morgan('tiny'));
 
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
 
 app.listen(3000, () => {
     console.log('App is running on localhost:3000')
