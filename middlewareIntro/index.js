31dce77a4cb8d62ab9fe467318b4c17be432eca1
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-10-02 13:09:04 -0400

    chaining middlewares

--- middlewareIntro/index.js
+++ middlewareIntro/index.js
@@ -11,6 +11,22 @@ app.use((req, res, next) => {
     next();
 })
 
+
+app.use((req, res, next) => {
+    console.log("THIS IS MY FIRST MIDDLEWARE!!!")
+    return next();
+    console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()")
+})
+app.use((req, res, next) => {
+    console.log("THIS IS MY SECOND MIDDLEWARE!!!")
+    return next();
+})
+app.use((req, res, next) => {
+    console.log("THIS IS MY THIRD MIDDLEWARE!!!")
+    return next();
+}) 
+
+
 app.listen(3000, () => {
     console.log('App is running on localhost:3000')
 })
