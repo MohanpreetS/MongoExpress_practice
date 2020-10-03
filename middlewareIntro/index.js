f8451d82e69be0cab6f802fac41ff0b672a5763b
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-10-03 13:09:04 -0400

    adding routes and a simple middleware

--- middlewareIntro/index.js
+++ middlewareIntro/index.js
@@ -12,19 +12,34 @@ app.use((req, res, next) => {
 })
 
 
-app.use((req, res, next) => {
-    console.log("THIS IS MY FIRST MIDDLEWARE!!!")
-    return next();
-    console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()")
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY FIRST MIDDLEWARE!!!")
+//     return next();
+//     console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()")
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY SECOND MIDDLEWARE!!!")
+//     return next();
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY THIRD MIDDLEWARE!!!")
+//     return next();
+// }) 
+
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!!")
+    next();
 })
-app.use((req, res, next) => {
-    console.log("THIS IS MY SECOND MIDDLEWARE!!!")
-    return next();
+
+app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('HOME PAGE!')
 })
-app.use((req, res, next) => {
-    console.log("THIS IS MY THIRD MIDDLEWARE!!!")
-    return next();
-}) 
+
+app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
+    res.send('WOOF WOOF!')
+})
 
 
 app.listen(3000, () => {
