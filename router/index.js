1a20a120e28e84713cd7974e3fa0377fc8b65ca9
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-11-24 13:09:04 -0500

    router intro

---
+++ router/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const shelterRoutes = require('./routes/shelters');
+const dogRoutes = require('./routes/dogs');
+const adminRoutes = require('./routes/admin')
+
+app.use('/dogs', dogRoutes);
+
+app.listen(3000, () => {
+    console.log('Serving app on localhost:3000')
+})
