a373d1c31e277d07f39b3b9265fd08fc23043314
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-11-26 13:09:04 -0500

    router with middleware

--- router/index.js
+++ router/index.js
@@ -6,6 +6,7 @@ const adminRoutes = require('./routes/admin')
 
 app.use('/shelters', shelterRoutes);
 app.use('/dogs', dogRoutes);
+app.use('/admin', adminRoutes)
 
 app.listen(3000, () => {
     console.log('Serving app on localhost:3000')
