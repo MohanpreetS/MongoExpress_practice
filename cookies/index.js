4e333bc8db5b5b8bfb168adcd2871da2b74d60f1
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-11-30 13:09:04 -0500

    signing and verifying cookies

--- cookies/index.js
+++ cookies/index.js
@@ -15,6 +15,16 @@ app.get('/setname', (req, res) => {
     res.send('OK SENT YOU A COOKIE!!!')
 })
 
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true })
+    res.send('OK SIGNED YOUR FRUIT COOKIE!')
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
+})
 
 app.listen(3000, () => {
     console.log("SERVING!")
