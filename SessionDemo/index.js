a68fe8fc2694243e92d1620cec4f5b811e6450e6
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-12-04 13:09:04 -0500

    retrieving session proprety values

--- SessionDemo/index.js
+++ SessionDemo/index.js
@@ -14,6 +14,17 @@ app.get('/viewcount', (req, res) => {
     res.send(`You have viewed this page ${req.session.count} times`)
 })
 
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet')
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`)
+})
+
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
