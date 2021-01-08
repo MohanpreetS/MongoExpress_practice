02124f0b216a458f36ed57765519c8011ffc7180
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-01-08 13:09:04 -0500

    mongo connect

---
+++ AuthDemo/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+
+
+
+mongoose.connect('mongodb://localhost:27017/loginDemo', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log("SERVING YOUR APP!")
+})
+
