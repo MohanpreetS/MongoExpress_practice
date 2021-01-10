80d304adc10489ca035c6e342a2da7ecdae58d92
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-01-10 13:09:04 -0500

    mlogin register routes

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -21,6 +21,28 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'notagoodsecret' }))
+
+app.get('/', (req, res) => {
+    res.send('THIS IS THE HOME PAGE')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const user = new User({ username, password })
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+
 
 app.listen(3000, () => {
     console.log("SERVING YOUR APP!")
