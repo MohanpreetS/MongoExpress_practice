a91f9b8b4859b29978cbfdfc30156c2c7778b265
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-01-12 13:09:04 -0500

    auth login logout api

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -23,6 +23,12 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(session({ secret: 'notagoodsecret' }))
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login')
+    }
+    next();
+}
 app.get('/', (req, res) => {
     res.send('THIS IS THE HOME PAGE')
 })
@@ -42,7 +48,30 @@ app.post('/register', async (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login')
 })
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/secret');
+    }
+    else {
+        res.redirect('/login')
+    }
+})
 
+app.post('/logout', (req, res) => {
+    req.session.user_id = null;
+    // req.session.destroy();
+    res.redirect('/login');
+})
+
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret')
+})
+app.get('/topsecret', requireLogin, (req, res) => {
+    res.send("TOP SECRET!!!")
+})
 
 app.listen(3000, () => {
     console.log("SERVING YOUR APP!")
